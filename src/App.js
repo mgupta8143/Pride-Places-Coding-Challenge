e1f6973641dfca01b89a1f8582f08516a8669dc0
mgupta8143 <mgupta8143@gmail.com>
2021-04-13 18:32:36 -0400

    Fetched API endpoints added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
-import './App.css';
+import React, { useState, useEffect } from 'react';
 import { Navbar } from './Components/NavBar/NavBar';
+import './App.css';
 
 function App() {
+  const [posts, setPosts] = useState([]);
+  const [users, setUsers] = useState([]);
+
+  const requestPosts = () => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then((response) => response.json())
+    .then((json) => setPosts(json))
+    .catch(() => console.log("error"));
+  };
+
+  const requestUsers = () => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response) => response.json())
+    .then((json) => setUsers(json))
+    .catch(() => console.log("error"));
+  }
+
+  useEffect(() => {
+      requestPosts();
+      requestUsers();
+  }, []);
+
+
   return (
-    <div class="body-wrap">
+    <div className="body-wrap">
       <Navbar/>
+      <ul>
+        {posts.map((val, index) => {
+          return <li key={index}>{val.title}</li>
+        })}
+      </ul>
+
+
+
     </div>
   );
 }
